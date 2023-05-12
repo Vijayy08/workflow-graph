96b0f0b0a71daa7fd423442c0143e7ca809b1700
Vijaya Vittala M <vvmfunkpac@gmail.com>
2023-05-12 21:32:55 +0530

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,144 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useCallback, useEffect, useState } from "react";
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+  useNodes,
+  useReactFlow,
+} from "reactflow";
 
-const inter = Inter({ subsets: ['latin'] })
+import "reactflow/dist/style.css";
+
+const initialNodes: any = [
+  // { id: '1', position: { x: 0, y: 0 }, data: { label: 'Cough or cold?' } },
+  // { id: '2', position: { x: -100, y: 100 }, data: { label: 'Next set of Qs' }},
+  // { id: '3', position: { x: 100, y: 100 }, data: { label: 'Next set of Qs' } }
+];
+
+const initialEdges:any = [
+  // { id: "e1-2", data: "cough", source: "1", target: "2", label: "cough" },
+  // { id: "e1-3", data: "cold", source: "1", target: "3", label: "cold" },
+];
 
 export default function Home() {
+  const [data, setData] = useState<any>();
+  const rf = useReactFlow();
+
+  //Fetched data from api
+  const addNode = () => {
+    
+    rf.addNodes({
+      id: (data?.response?.nodeDto?.questionContent?.id).toString(),
+      position: { x: 0, y: 0 },
+      data: {
+        label: (data?.response?.nodeDto?.questionContent?.content).toString(),
+      },
+    });
+    
+    var x = -400;
+    var y = 400;
+    (data?.response.nodeDto.edgeDtoList).forEach((ele: any, idx :number) => {
+      // console.log(ele.nodeDto.questionContent);
+      let id = `${idx}-${ele.questionContent?.id}`
+      rf.addNodes(
+        {
+        'id': id,
+        position: {
+          x: (x +=200),
+          y: (y),
+        },
+        data: { label: ele.nodeDto.questionContent.content},
+      });
+
+
+      y=800;
+      // x=-400;
+      (ele.nodeDto.questionContent.answerOptionList).forEach((element:any )=> {
+        rf.addNodes(
+          {
+          'id': `${id}-${element.id}`,
+          position: {
+            x: (x+=200),
+            y: (y),
+          },
+          data: { label: element.content},
+        });
+        rf.addEdges(
+          { 'id': `${id}-${element.id}`, 'source':`${id}` , 'target': `${id}-${element.id}` }
+        )
+      });
+
+
+      // x= -400;
+      y=400;
+      rf.addNodes(
+          {
+            'id': id,
+            position: {
+              x: (x+=200),
+              y: (y),
+            },
+            data: { label: ele.nodeDto.questionContent.answerOptionList},
+          
+      }
+      )
+
+
+
+      console.log(ele.conditionDto.answerOptionDtoList[0].content);
+      rf.addEdges(
+        { 'id': `${data?.response?.nodeDto?.questionContent?.id}-${id}`, 'source':`${data?.response?.nodeDto?.questionContent?.id}` , 'target': id, label: `${ele.conditionDto.answerOptionDtoList[0].content}` }
+      )
+      
+    });
+
+    // ((data?.response.nodeDto.questionContent.answerOptionList).forEach((element:any) => {
+    //   console.log(element.content)
+    //   rf.addNodes({ id: `${data?.response?.nodeDto?.questionContent?.id}-${element.content}`, data:'cough','source': id.toString(), target: element.content, label:element.content })
+    // }))
+  };
+  const fetchData = async () => {
+    const apiUrl = `http://3.13.92.74:30005/questionnaire/admin/question-workflow/id/f1745111-d251-444c-bd9d-63e19615b551`;
+
+    await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "X-USER-ID": "test",
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res.response);
+        setData(res);
+        addNode();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="" style={{ width: "100vw", height: "100vh" }}>
+      <button className="bg-blue-500 text-white font-xl" onClick={fetchData}>
+        Fetch
+      </button>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      ></ReactFlow>
+    </div>
+  );
 }
---
+++ pages/useNodes.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import ReactFlow, { useNodesState, useEdgesState, addEdge, Connection, Edge } from 'reactflow';
+
+// function useNodes() {
+//     const initialNodes = [
+//         { id: '1', position: { x: 0, y: 0 }, data: { label: 'Cough or cold?' } },
+//         { id: '2', position: { x: -100, y: 100 }, data: { label: 'Next set of Qs' }},
+//         { id: '3', position: { x: 100, y: 100 }, data: { label: 'Next set of Qs' } }
+//       ];
+// }
+
+// export default useNodes
